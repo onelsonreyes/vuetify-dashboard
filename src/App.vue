302214614a11915fc938635861cc42161fd0ab94
onelson <onelson.reyes@gmailcom>
2020-03-22 11:27:06 -0600

    add login form

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <v-app>
-  <v-card>
+  <v-card width="400" class="mx-auto mt-5">
     <v-card-title>
       <h1 class="display-1">Login</h1>
     </v-card-title>
     <v-card-text>
       <v-form>
-        <v-text-field label="Username" />
         <v-text-field 
-          type="password"
-          label="Password"  />
+            label="Username"
+            prepend-icon="mdi-account-circle"
+        />
+        <v-text-field 
+          :type="showPassword ? 'text' : 'password'"
+          label="Password"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"
+        />
       </v-form>
     </v-card-text>
+    <v-divider></v-divider>
     <v-card-actions>
-      <v-btn>Register</v-btn>
-      <v-btn>Login</v-btn>
+      <v-btn color="success">Register</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="info">Login</v-btn>
     </v-card-actions>
   </v-card>
   </v-app>
@@ -31,7 +40,7 @@ export default {
   },
 
   data: () => ({
-    //
+    showPassword: false
   }),
 };
 </script>
